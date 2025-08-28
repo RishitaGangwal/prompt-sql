c2b56bc6a23a2839dc7768c102c361a4d5cc537c
RishitaGangwal <rishitagangwal5@gmail.com>
2025-08-28 21:51:01 +0530

    added changes

--- server/src/main/java/com/promptsql/server/config/WebConfig.java
+++ server/src/main/java/com/promptsql/server/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "https://prompt-sql.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
